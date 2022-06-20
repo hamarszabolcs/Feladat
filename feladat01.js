2c982bdfb84b991b4872bee6b788d83facf28052
hamarszabolcs <107812719+hamarszabolcs@users.noreply.github.com>
2022-06-20 15:16:20 +0200

    Create feladat01.js

---
+++ feladat01.js
@@ -0,0 +1,13 @@
+export function feladat01(szinek){
+    if(szinek.length <= 1)
+    {
+        return 0;
+    }else if(szinek.length === 2)
+    {
+        return 1;
+    }else{
+        return 4;
+    
+    
+    }
+}
